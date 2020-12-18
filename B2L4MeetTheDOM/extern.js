53668fac9ee7132af8420807269b40813fc4f8bc
Ryan de Fost <ryandefost@hotmail.com>
2020-12-18 13:08:07 +0100

    MakeAscene MeetDOM turtle add

---
+++ B2L4MeetTheDOM/extern.js
@@ -0,0 +1,8 @@
+
+function clickedMe(){
+    document.getElementById("greet").textContent = "Thank you for using the DOM externally";
+}
+
+function changeTitle(){
+    document.getElementById("greet").title = "YEET!";
+}
