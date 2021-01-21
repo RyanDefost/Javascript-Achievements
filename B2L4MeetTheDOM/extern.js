b428027189182c10db1af6d59b717ac2bbb690cf
Ryan de Fost <ryandefost@hotmail.com>
2021-01-21 09:46:03 +0100

    Peepshow gemaakt

--- B2L4MeetTheDOM/extern.js
+++ B2L4MeetTheDOM/extern.js
@@ -2,7 +2,3 @@
 function clickedMe(){
     document.getElementById("greet").textContent = "Thank you for using the DOM externally";
 }
-
-function changeTitle(){
-    document.getElementById("greet").title = "YEET!";
-}
